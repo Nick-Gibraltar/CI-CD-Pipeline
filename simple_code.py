42b29764f2da4274dfda2671d7264a92277020ab
Nick <nicholas.gibbins@blueyonder.co.uk>
2023-01-13 21:12:01 +0000

    Adds some very simple code

---
+++ simple_code.py
@@ -0,0 +1,7 @@
+import numpy as py
+print("here is some very simple code")
+print("it imports numpy just for the sake of it")
+for i in range(100):
+    for j in range(100):
+        print(i*j)
+print("that's it")
